334e86ff60c1df7712499bbe1872fc934ba1f03e
Oliveiras96 <caique.campos@aluno.ufabc.edu.br>
2021-01-21 11:10:39 -0300

    adds the snake

--- js/script.js
+++ js/script.js
@@ -8,12 +8,29 @@ let context = canvas.getContext("2d");
 // Dimensão das caixinhas: 32x32 px
 let box = 32;
 
+// cobrinha  vai ser um array de coordenadas dentro do canvas:
+let snake = [];
+snake[0] = {
+    x: 8 * box,
+    y: 8 * box
+}
+
 // Função para criar o canvas de fato:
 function criarBG(){
     context.fillStyle = "lightgreen";
     context.fillRect(0, 0, 16 * box, 16 * box)
 }
 
-criarBG();
+// A cobrinha anda pela adição de um elemento em um array e pela remoção do primeiro
+function criarCobrinha() {
+    for (i = 0; i < snake.length; i++) {
+        // cor da cobrinha
+        context.fillStyle = "green";
+        // posiciona a cobrinha no canvas:
+        context.fillRect(snake[i].x, snake[i].y, box, box);
+        
+    }
+}
 
-// TODO: Criar cobrinha
+criarBG();
+criarCobrinha();
