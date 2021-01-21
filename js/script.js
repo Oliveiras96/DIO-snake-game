f5dfcc2f076f1ddb4ca2942353937e2a0b3954e7
Oliveiras96 <caique.campos@aluno.ufabc.edu.br>
2021-01-21 10:42:54 -0300

    adds background

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// TODO: Criar o canvas
+
+// obter o elemento correspondente ao canvas
+let canvas = document.getElementById("snake");
+
+// explicitar que é um canvas  2D:
+let context = canvas.getContext("2d");
+
+// Dimensão das caixinhas: 32x32 px
+let box = 32;
+
+// Função para criar o canvas de fato:
+function criarBG(){
+    context.fillStyle = "lightgreen";
+    context.fillRect(0, 0, 16 * box, 16 * box)
+}
+
+criarBG();
